ba14ef4131e04f9a72116f8bc6b9b3ff0f3f8283
Vihan Patidar <123254447+vihan16@users.noreply.github.com>
2024-05-18 10:34:07 +0000

    01 variable basic done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 144553
+let accountEmail = "vihan@abcd.com"
+var accountPassword = "12345"
+accountCity = "indore"
+
+accountEmail = "vih@abcd.com"
+accountPassword = "121212"
+accountCity = "delhi"
+
+console.log(accountId);
+console.table({accountId, accountEmail, accountPassword, accountCity})
