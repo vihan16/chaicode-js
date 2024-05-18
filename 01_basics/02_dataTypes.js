9189ee623c94a52afaaffe705a2a69bd7b422b54
Vihan Patidar <123254447+vihan16@users.noreply.github.com>
2024-05-18 12:25:55 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,15 @@
+"use strict" // this treats all js as new version
+
+//alert(3 + 3) // we are using node js not browser
+
+console.log("vihan")
+
+let name = "vihan"
+let age = "20"
+let isLogedIn = "true"
+
+//boolean
+//null => stand alone value
+//undefined 
+//symbol => unique
+//bigint 
