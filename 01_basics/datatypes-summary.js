2a091a7f243e2d2576ab78f237de73a446def447
Vihan Patidar <123254447+vihan16@users.noreply.github.com>
2024-05-25 12:16:17 +0000

    done

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,27 @@
+// primitive 
+
+// 7 types - BigInt, Number, Boolean, String , null, Undefined, symbol
+
+// Reference (non pr)
+
+const score = 100
+const scoreValue = 100.3
+const isLoggedIn = true
+const tempOutside = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+console.log(id === anotherId)
+
+// array , object , function
+const heros = ["abc","efg","hij"]
+
+let myObj = {
+    name : "vihan",
+    age: 21,
+}
+
+const myFunction = function(){
+    console.log("my World");
+}
