11ff3e78cc8f3fdd059717f2de264e4c51f29d92
Vihan Patidar <123254447+vihan16@users.noreply.github.com>
2024-05-25 17:52:32 +0000

    dn 2

--- 01_basics/06_num-math.js
+++ 01_basics/06_num-math.js
@@ -1,5 +1,27 @@
 const score = 400
-console.log(score)
+//console.log(score)
 
 const balance = new Number(222)
-console.log(balance)
+//console.log(balance)
+
+//console.log(balance.toString().length)
+//console.log(balance.toFixed(2))
+
+const otherNumber = 123.987
+//console.log(otherNumber.toPrecision(4))
+
+const hundreds = 1000000
+//onsole.log(hundreds.toLocaleString('en-IN'))
+
+
+//----------------- MATHS -------------------------
+
+//console.log(Math.abs(-4))
+//console.log(Math.round(4.6))
+//console.log(Math.ceil(4.2))
+//console.log(Math.floor(4.9))
+//console.log(Math.min(4,3,6,8,9))
+
+console.log(Math.random())
+console.log((Math.random()*10) + 1)
+
