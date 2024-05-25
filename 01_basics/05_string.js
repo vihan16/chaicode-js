7bb4d334979b3bebcac8f641f03fa950ef30d185
Vihan Patidar <123254447+vihan16@users.noreply.github.com>
2024-05-25 14:29:59 +0000

    done

---
+++ 01_basics/05_string.js
@@ -0,0 +1,26 @@
+ const name = "vihan"
+ const repoName = 50
+
+ //console.log(name + repoName + "uno")
+
+ console.log(`hello my name is ${name} my repo name is ${repoName}`)
+
+
+ const gameName = new String(`vih-aan`)
+ //console.log(gameName[1])
+ //console.log(gameName.length)
+ //console.log(gameName.charAt(2))
+ //console.log(gameName.indexOf('i'))
+
+const newString = gameName.substring(0,4)
+console.log(newString)
+
+const anotherString = gameName.slice(-6, 3)
+console.log(anotherString)
+
+const anoString = "    vihan   "
+console.log(anoString);
+console.log(anoString.trim())
+
+const url = "https://hdunibfb.com/nciin%48vihd"
+console.log(url.replace('%48' , '--'))
