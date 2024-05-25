7cc14a3d4f751955cc02f315b6ef95276318f085
Vihan Patidar <123254447+vihan16@users.noreply.github.com>
2024-05-25 08:51:52 +0000

    done

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,10 @@
+console.log("2" > 1)
+console.log("02" > 1)
+
+console.log(null > 0)
+console.log(null == 0)
+console.log(null >= 0)
+
+
+//equality check == and comparision works >< >= <= different  
+//comparision convert null to a number , treating it as 0
