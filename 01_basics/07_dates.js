b227ae47c09ee2af0d892e0b6b87d7328d9233fe
Vihan Patidar <123254447+vihan16@users.noreply.github.com>
2024-05-26 06:35:19 +0000

    date done

---
+++ 01_basics/07_dates.js
@@ -0,0 +1,18 @@
+let myDate = new Date()
+//console.log(myDate.toString())
+//console.log(myDate.toDateString())
+//console.log(myDate.toLocaleString())
+
+let myCreatedDate = new Date("01-15-24")
+//console.log(myCreatedDate.toLocaleString())
+
+
+let myTimeStamp = Date.now();
+
+//console.log(myTimeStamp);
+//console.log(myCreatedDate.getTime())
+
+let newDate = new Date()
+console.log(newDate)
+console.log(newDate.getMonth())
+console.log(newDate.getDay())
