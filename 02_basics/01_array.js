39cd3ad9bf6476b720c792eab86a202f17d7cb71
Vihan Patidar <123254447+vihan16@users.noreply.github.com>
2024-05-26 11:32:48 +0000

    done

--- 02_basics/01_array.js
+++ 02_basics/01_array.js
@@ -6,9 +6,24 @@ console.log(arr2[1])
 
 //array method 
 
-myArr.push(4)
-myArr.push(6)
-myArr.unshift(8)
-myArr.shifft()
+//myArr.push(4)
+//myArr.push(6)
+//myArr.unshift(8)
+//myArr.shifft()
 
-console.log(myArr)
+//console.log(myArr.includes(3))
+//console.log(myArr.indexOf(3))
+
+//console.log(myArr)
+
+//slice , splice
+
+console.log("A ", myArr)
+const myn1 = myArr.slice(1, 3)
+
+console.log(myn1)
+console.log("B ", myArr)
+
+const myn2 = myArr.splice(1 ,3)
+console.log("C", myArr)
+console.log(myn2)
