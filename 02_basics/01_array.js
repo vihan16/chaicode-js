d2ff8f1791dc4c24e61d931abc9f8db74dd02450
Vihan Patidar <123254447+vihan16@users.noreply.github.com>
2024-05-26 09:09:45 +0000

    half done

---
+++ 02_basics/01_array.js
@@ -0,0 +1,14 @@
+const myArr = [0,1,2,3,5]
+const myHero = ["abc" , "def"]
+const arr2 = new Array(2, 3, 4, 7)
+console.log(myArr[0])
+console.log(arr2[1])
+
+//array method 
+
+myArr.push(4)
+myArr.push(6)
+myArr.unshift(8)
+myArr.shifft()
+
+console.log(myArr)
