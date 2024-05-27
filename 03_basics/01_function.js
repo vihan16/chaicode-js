58a09358b0243d708eb7ec35c8fc8aa584668191
Vihan Patidar <123254447+vihan16@users.noreply.github.com>
2024-05-27 10:27:54 +0000

    done2

--- 03_basics/01_function.js
+++ 03_basics/01_function.js
@@ -16,9 +16,35 @@ function addTwoNumbers(number1, number2){
 }
 
 const result = addTwoNumbers(3, 5)
-console.log("Result: ", result);
+//console.log("Result: ", result);
 
 function loginUserMessage(username){
     return `${username} just logged in`
 }
-console.log(loginUserMessage("vihan"))
+//console.log(loginUserMessage("vihan"))
+
+function calculateCartPrice(...num1){
+    return num1
+}
+
+console.log(calculateCartPrice(200, 400, 1000 , 2500))
+
+const user = {
+    username: "vihan",
+    price: 100
+
+}
+
+function handleObject(anyObject){
+    console.log(`username is ${anyObject.username} and price is ${anyObject.price}`)
+
+}
+
+handleObject(user)
+
+const myArray = [200, 200 , 400,800]
+function returnSecondValue(getArray){
+    return getArray[3]
+
+}
+console.log(returnSecondValue(myArray))
