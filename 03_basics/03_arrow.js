b5d3745aef3998ea9307c7776127bc3ba8384c1b
Vihan Patidar <123254447+vihan16@users.noreply.github.com>
2024-05-28 18:42:34 +0000

    done

--- 03_basics/03_arrow.js
+++ 03_basics/03_arrow.js
@@ -12,3 +12,15 @@ user.username = "vvvv"
 user.welcomemsg()
 
 console.log(this)
+
+// function chai(){
+//     let username = "vihan"
+//     console.log(this.username)
+// }
+
+// chai()
+
+const addTwo = (num1, num2) => {
+    return num1 + num2
+}
+console.log(addTwo(3, 4))
