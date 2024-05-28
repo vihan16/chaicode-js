37de06136253b326e0be76e01c55488b104685c3
Vihan Patidar <123254447+vihan16@users.noreply.github.com>
2024-05-28 07:34:40 +0000

    done 2

--- 03_basics/02_scope.js
+++ 03_basics/02_scope.js
@@ -5,9 +5,32 @@ let a = 400
 if (true) {
     let a = 100
     const b = 20
-    console.log("Inner:", a)
+    //console.log("Inner:", a)
 }
 
-console.log(a)
+//console.log(a)
 //console.log(b)
 //console.log(c)
+
+function one(){
+    const username = "vihan"
+
+    function two(){
+        const website = "utube"
+        console.log(username)
+    }
+    //console.log(website)
+
+    two()
+}
+//one()
+
+if (true){
+    const username = "vihan"
+    if (username === "vihan"){
+        const website = "utube"
+        console.log(username + website)
+    }
+    //confirm.log(website)
+}
+//console.log(username)
