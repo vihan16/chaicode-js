9e2752caf0210338fa00564abe529a641bc8f0f7
Vihan Patidar <123254447+vihan16@users.noreply.github.com>
2024-05-29 12:30:27 +0000

    done

---
+++ 04_controlflow/02_switch.js
@@ -0,0 +1,17 @@
+const month = 3
+
+switch (month) {
+    case 1:
+        console.log("Jan")
+        break;
+    case 2:
+        console.log("Feb")
+        break;
+    case 3:
+        console.log("mar")
+        break;
+   
+    default:
+        console.log("default case match")
+        break;
+}
