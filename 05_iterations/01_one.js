59178c8c09a0b987cd9c2c36ea281ccd90dbdd0c
Vihan Patidar <123254447+vihan16@users.noreply.github.com>
2024-05-30 08:01:08 +0000

    done

--- 05_iterations/01_one.js
+++ 05_iterations/01_one.js
@@ -6,8 +6,33 @@ for (let index = 0; index < 10; index++) {
 }
 
 for (let i = 0; i < 5; i++) {
-     console.log(`Outer loop value : $(i)`);
+    // console.log(`Outer loop value : $(i)`);
     for (let j = 0; j < 5; j++) {
-     console.log(`iner loop valie $(j) and inner loop $(i)`)
+     //console.log(`iner loop valie $(j) and inner loop $(i)`)
     } 
+}
+let myArray = ["flash", "batman ", "superman"]
+//console.log(myArray.length)
+for (let index = 0; index < myArray.length; index++) {
+    const element = myArray[index];
+    //console.log(element)
+}
+
+//break and continue
+
+// for (let index = 1; index <= 20; index++) {
+//     if(index == 5){
+//         console.log(`Detected 5`)
+//         break
+//     }
+//     console.log(`Value of i is ${index}`);
+    
+// }
+for (let index = 1; index <= 20; index++) {
+    if(index == 5){
+        console.log(`Detected 5`)
+        continue
+    }
+    console.log(`Value of i is ${index}`);
+    
 }
