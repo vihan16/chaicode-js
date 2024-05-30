f5aebcffa1b7918b5c1fb03f6809ebf4f89fbdda
Vihan Patidar <123254447+vihan16@users.noreply.github.com>
2024-05-30 14:07:29 +0000

    done

---
+++ 05_iterations/three.js
@@ -0,0 +1,18 @@
+// for of
+
+const arr = [1, 2, 3 , 4 ,5]
+for (const num of arr) {
+   // console.log(num);
+    
+}
+
+const map = new Map()
+map.set('IN', "India")
+map.set('USA', "united states of an=merica")
+map.set('fr', 'France')
+
+//console.log(map)
+for (const [naksha,value] of map) {
+   // console.log(naksha ,':-', value)
+}
+
