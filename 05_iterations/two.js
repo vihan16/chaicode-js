0e50f1597c8157e42018ab34b20366dd1a01a0bd
Vihan Patidar <123254447+vihan16@users.noreply.github.com>
2024-05-30 08:53:39 +0000

    done

---
+++ 05_iterations/two.js
@@ -0,0 +1,20 @@
+let index = 0
+while (index <= 10){
+    //console.log(`Value of index is ${index}`)
+    index = index + 2
+}
+
+let myArray = ["nobita", "gian", "suneo", "sizuka", "dorimon"]
+
+let arr = 0
+while (arr < myArray.length) {
+    //console.log(`names are: ${myArray[arr]}`)
+    arr = arr + 1
+}
+
+let score = 1
+
+do {
+    console.log(`score is: ${score}`)
+    score++
+} while (score < 10);
