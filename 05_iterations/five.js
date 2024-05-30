72278a850ed917f85bb3b76cd8f238c2a13f735e
Vihan Patidar <123254447+vihan16@users.noreply.github.com>
2024-05-30 15:11:17 +0000

    foeEach done

---
+++ 05_iterations/five.js
@@ -0,0 +1,31 @@
+const coding = ["js", "ruby", "java", "python", "cpp"]
+coding.forEach(function(val){
+    //console.log(val);
+})
+
+function printMe(item){
+    //console.log(item);
+}
+//coding.forEach(printMe)
+
+coding.forEach( (item, index, arr)=> {
+    //console.log(item, index, arr)
+})
+
+const myCoding = [
+    {
+        languageName: "javascript",
+        languageFileName: "js"
+    },
+    {
+        languageName: "python",
+        languageFileName: "py"
+    },
+    {
+        languageName: "c++",
+        languageFileName: "cpp"
+    },
+]
+myCoding.forEach((item) => {
+    console.log(item.languageName);
+})
